6a860b4ee6a6c8d7de5f7da31f7627b26cc5131f
anguyn <19521185@gm.uit.edu.vn>
2022-05-21 23:44:27 +0700

    Add Admin, Guest, User Header and edit MainLayout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,14 @@
-import Topbar from '../components/Header/Topbar';
-import Footer from '../components/Footer/Footer';
+import GuestTopBar from '../components/Header/GuestTopBar/index';
+import UserTopBar from '../components/Header/UserTopBar/index';
+import AdminTopBar from '../components/Header/AdminTopBar/index';
+import Footer from '../components/Footer/index';
 
+
+// Có 3 loại Top bar: Khách, User, Admin
 export default function MainLayout({ children }) {
     return (
         <>
-            <Topbar />
+            <UserTopBar />
             {children}
             <Footer />
         </>
