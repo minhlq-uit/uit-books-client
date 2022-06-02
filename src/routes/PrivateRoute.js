616a30b73c2b42fd2c80e51c3d827c235347f4b3
nnmchau <19520023@gm.uit.edu.vn>
2022-06-02 21:33:25 +0700

    fix css

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -4,12 +4,6 @@ import { Navigate, Route } from "react-router-dom";
 
 const ProtectedRoute = ({ isAdmin, children }) => {
   const { user } = useSelector((state) => state.user);
-<<<<<<< HEAD
- console.log('children', children)
- console.log('is admin user role', isAdmin, user.role)
-=======
-
->>>>>>> 48df1ab36715c232db2116b88a3706ac838860de
   return (
     <>
       {/* { !user && <Navigate to="/signin" /> }
